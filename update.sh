6514aeaef2b42927992fcde557d2fae988cfde01
Jason Morris <jason@roundtablelaw.ca>
2020-06-06 00:13:23 -0600

    Adding documentation to update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,12 @@
+# update.sh
+#
+# Script for copying revised development blawx files into a running docker container
+# using the blawx image.
+#
+# This script assumes that there is one running docker container on the development
+# machine that uses a version of the blawx image. If there is more than one, this
+# script will probably throw errors.
+
 docker cp ./interface/. $(docker ps -qf "ancestor=blawx"):/var/www/html
 docker cp ./reasoner/reasoner.php $(docker ps -qf "ancestor=blawx"):/usr/lib/cgi-bin
 docker cp ./reasoner/decode.js $(docker ps -qf "ancestor=blawx"):/var/www/html
