1738b7a7ca17f00ecacd055e861f13cf091a6cde
Jason Morris <jason@roundtablelaw.ca>
2021-01-30 09:15:48 +0000

    adding customized blocks to attributes callback

--- interface/categories.js
+++ interface/categories.js
@@ -15,7 +15,24 @@ knownAttributesCallback = function(workspace) {
         var attribute_name = blockList[i].getFieldValue('attribute_name');
         var attribute_type = blockList[i].getInputTargetBlock('attribute_type').toString();
         attributeTypes[attribute_name] = attribute_type;
-        var blockText = "<xml><block type='attribute_selector'><field name='attributeName'>" + attribute_name + "</field></block></xml>"
+        // At this point, I need to check to see if the next block is a customization.
+        var nextblock = blocklist[i].getNextBlock();
+        if (nextblock && nextblock.type == "custom_attribute_declaration") {
+            // If it is, I need to get (where the next block is "block")
+            var dropdown_order = nextblock.getFieldValue('order');
+            var text_prefix = nextblock.getFieldValue('prefix');
+            var text_infix = nextblock.getFieldValue('infix');
+            var text_postfix = nextblock.getFieldValue('postfix');
+            // And then I need to construct the custom block, in such a way as to give it
+            // the information it needs to set the TYPE checking value on both fields.
+            if (dropdown_order == "object_first") {
+                var blockText = "<xml><block type='custom_attribute_selector'><field name='prefix'>" + text_prefix + "</field><field name='infix'>" + text_infix + "</field><field name='postfix'>" + text_postfix + "</field></block></xml>";
+            } else {
+                var blockText = "<xml><block type='custom_attribute_selector'><field name='prefix'>" + text_prefix + "</field><field name='infix'>" + text_infix + "</field><field name='postfix'>" + text_postfix + "</field></block></xml>";
+            }
+        } else {
+            var blockText = "<xml><block type='attribute_selector'><field name='attributeName'>" + attribute_name + "</field></block></xml>";
+        }
         var block = Blockly.Xml.textToDom(blockText).firstChild;
         // need to check for repeatedly adding the same block.
         xmlList.push(block);
@@ -25,11 +42,33 @@ knownAttributesCallback = function(workspace) {
     var blockList = importDictionary[id].getAllBlocks();
     
     for (var i = 0; i< blockList.length; i++) {
-        if (blockList[i].type == "attribute_declaration") {
+        if (blockList[i].type == "attribute_declaration" |
+        blockList[i].type == "cardinality_up_to" |
+        blockList[i].type == "cardinality_or_more" |
+        blockList[i].type == "cardinality_between" |
+        blockList[i].type == "cardinality_exactly" |
+        blockList[i].type == "cardinality_any") {
         var attribute_name = blockList[i].getFieldValue('attribute_name');
         var attribute_type = blockList[i].getInputTargetBlock('attribute_type').toString();
         attributeTypes[attribute_name] = attribute_type;
-        var blockText = "<xml><block type='attribute_selector'><field name='attributeName'>" + attribute_name + "</field></block></xml>"
+        // At this point, I need to check to see if the next block is a customization.
+        var nextblock = blocklist[i].getNextBlock();
+        if (nextblock && nextblock.type == "custom_attribute_declaration") {
+            // If it is, I need to get (where the next block is "block")
+            var dropdown_order = nextblock.getFieldValue('order');
+            var text_prefix = nextblock.getFieldValue('prefix');
+            var text_infix = nextblock.getFieldValue('infix');
+            var text_postfix = nextblock.getFieldValue('postfix');
+            // And then I need to construct the custom block, in such a way as to give it
+            // the information it needs to set the TYPE checking value on both fields.
+            if (dropdown_order == "object_first") {
+                var blockText = "<xml><block type='custom_attribute_selector'><field name='prefix'>" + text_prefix + "</field><field name='infix'>" + text_infix + "</field><field name='postfix'>" + text_postfix + "</field></block></xml>";
+            } else {
+                var blockText = "<xml><block type='custom_attribute_selector'><field name='prefix'>" + text_prefix + "</field><field name='infix'>" + text_infix + "</field><field name='postfix'>" + text_postfix + "</field></block></xml>";
+            }
+        } else {
+            var blockText = "<xml><block type='attribute_selector'><field name='attributeName'>" + attribute_name + "</field></block></xml>";
+        }
         var block = Blockly.Xml.textToDom(blockText).firstChild;
         // need to check for repeatedly adding the same block.
         xmlList.push(block);
@@ -41,6 +80,8 @@ knownAttributesCallback = function(workspace) {
 
 demoWorkspace.registerToolboxCategoryCallback('KNOWN_ATTRIBUTES', knownAttributesCallback);
 
+// New Known Attributes
+// Needs to put the 
 
 var knownObjectsCallback;
 knownObjectsCallback = function(workspace) {
