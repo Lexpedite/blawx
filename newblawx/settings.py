3d34c62780fec6097f0d8b4148eda9979795d3a0
Jason Morris <jason@lexpedite.ca>
2022-01-24 11:38:30 -0700

    moving configuration into app directory

--- newblawx/settings.py
+++ newblawx/settings.py
@@ -57,7 +57,6 @@ ROOT_URLCONF = 'newblawx.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
