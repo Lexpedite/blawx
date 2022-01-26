6ce3b68d517f8be1e23b0cdfd7389bb990334c5a
Jason Morris <jason@lexpedite.ca>
2022-01-26 12:33:09 -0700

    adding workspaces to admin app

--- blawx/admin.py
+++ blawx/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Workspace
+
+class WorkspaceAdmin(admin.ModelAdmin):
+    fields = ['workspace_name','xml_content','scasp_encoding','workspace_example']
+
+admin.site.register(Workspace,WorkspaceAdmin)
