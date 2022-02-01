4202835c21a590428fa529e2b2f429e088e799b7
Jason Morris <jason@lexpedite.ca>
2022-02-01 13:21:06 -0700

    removing polls and adjusting urls to root

--- newblawx/settings.py
+++ newblawx/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'blawx.apps.BlawxConfig',
     'django.contrib.admin',
     'django.contrib.auth',
