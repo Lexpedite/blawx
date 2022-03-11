b0cb34b8189ae3da2bf21a8adee114bf751cde01
Gauntlet173 <jason@lexpedite.ca>
2022-03-10 23:17:11 -0700

    correctin module name

--- blawx/models.py
+++ blawx/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from .parse_an import generate_tree
 from cobalt.hierarchical import Act
-from clean import generate_akn
+from clean.clean import generate_akn
 
 # Create your models here.
 class RuleDoc(models.Model):
