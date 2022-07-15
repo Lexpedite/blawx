75fb553eb58f5c6239527005c43a7ac8d4d71634
Gauntlet173 <jason@lexpedite.ca>
2022-07-15 11:44:10 -0600

    create permissions when objects are saved

--- blawx/signals.py
+++ blawx/signals.py
@@ -1,8 +1,39 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 from guardian.shortcuts import assign_perm, remove_perm
 from guardian.utils import get_anonymous_user
-from .models import RuleDoc
+from .models import RuleDoc, BlawxTest, Workspace
+
+@receiver(post_save, sender=User)
+def user_post_save(sender, **kwargs):
+    """
+    When a user is created, other than anonymous, give them permission to create ruledocs.
+    """
+    user = kwargs['instance']
+    if user != get_anonymous_user():
+        assign_perm('add_ruledoc')
+
+@receiver(post_save,sender=BlawxTest)
+def blawxtest_post_save(sender, **kwargs):
+    """
+    When a test is created, give the owner permission to change, view, run, and delete it.
+    """
+    test = kwargs['instance']
+    perms = ['view_blawxtest','change_blawxtest','delete_blawxtest','run']
+    for p in perms:
+        assign_perm(p,test.ruledoc.owner) #Interested to see if this works.
+
+@receiver(post_save,sender=Workspace)
+def workspace_post_save(sender, **kwargs):
+    """
+    When a workspace is created, give the owner permission to change, view, and delete it.
+    """
+    workspace = kwargs['instance']
+    perms = ['view_workspace','change_workspace','delete_workspace']
+    for p in perms:
+        assign_perm(p,workspace.ruledoc.owner)
+
 
 @receiver(post_save, sender=RuleDoc)
 def ruledoc_post_save(sender, **kwargs):
@@ -33,6 +64,9 @@ def ruledoc_post_save(sender, **kwargs):
     test_permissions = ['view_blawxtest','add_blawxtest','change_blawxtest','delete_blawxtest','run']
     for rp in ruledoc_permissions:
         assign_perm(rp,ruledoc.owner,ruledoc)
+    # Resetting the permissions on all the associated objects MIGHT be necessary if the owner was the thing that changed,
+    # so we are doing it here as boots and suspenders sort of thing.
+    # There is probably a better way.
     for w in ruledoc.workspaces.all():
         for wp in workspace_permissions:
             assign_perm(wp,ruledoc.owner,w)
