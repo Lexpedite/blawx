ea7b67ba32fdff9862e68a1357cc45451519d49f
Gauntlet173 <jason@lexpedite.ca>
2023-05-15 17:13:40 -0600

    make simplify dependent on API key

--- blawx/simplifier.py
+++ blawx/simplifier.py
@@ -7,13 +7,21 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from rest_framework.permissions import IsAuthenticated, DjangoObjectPermissions, IsAuthenticatedOrReadOnly, AllowAny
 
 import openai
+import os
 
 prompt_preamble = """
+What follows is an automatically generated explanation. Restate it in plain language without restating mathematical calculations and
+without further justifying conclusions for which there is only an absence of evidence in support.
+
+
 """
 
 @api_view(['POST'])
 @authentication_classes([SessionAuthentication])
 @permission_classes([IsAuthenticated])
 def simplify(request):
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt_preamble + request.data['explanation'] }])
-    return Response(completion.choices[0].message.content)
+    if "OPENAI_API_KEY" in os.environ:
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt_preamble + request.data['explanation'] }])
+        return Response(completion.choices[0].message.content)
+    else:
+        return Response("")
